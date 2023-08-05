33a4c0929ed1894a059e328c7452e5fcdb220e67
Zach Yam <zyam@zyam-mn2.linkedin.biz>
2023-08-05 01:13:33 -0700

    First push

--- src/App.js
+++ src/App.js
@@ -1,24 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import React from "react";
+import {
+  MDBRow,
+  MDBCol,
+  MDBInput,
+  MDBContainer,
+  MDBBtn,
+} from "mdb-react-ui-kit";
+import CodeRegulations from "./components/CodeRegulations";
+import ZoningRegulations from "./components/ZoningRegulations";
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import "@fortawesome/fontawesome-free/css/all.min.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MDBRow style={{ paddingTop: '75px', paddingLeft: '100px', margin: 'auto'}}>
+        <MDBCol md="6">
+          <form>
+            <div className="grey-text">
+              <MDBInput label="Project Address" icon="user" group type="text" validate error="wrong" success="right" />
+              <MDBInput label="Project Number" icon="envelope" group type="email" validate error="wrong" success="right" />
+              <MDBInput label="Project Applicant" icon="tag" group type="text" validate error="wrong" success="right" />
+            </div>
+          </form>
+        </MDBCol>
+        <MDBCol md="3">
+          <form>
+            <div className="grey-text">
+              <MDBInput type="APN" label="APN" icon="pencil-alt" />
+            </div>
+          </form>
+        </MDBCol>
+      </MDBRow>
+      <CodeRegulations/>
+      <ZoningRegulations/>
     </div>
+    
   );
 }
 
---
+++ src/components/ZoningRegulations.js
@@ -0,0 +1,80 @@
+import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell, { tableCellClasses } from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+const StyledTableCell = styled(TableCell)(({ theme }) => ({
+    [`&.${tableCellClasses.head}`]: {
+        backgroundColor: '#156FA6',
+        color: theme.palette.common.white,
+        fontSize: 18
+    },
+    [`&.${tableCellClasses.body}`]: {
+        fontSize: 16,
+    },
+}));
+
+const StyledTableRow = styled(TableRow)(({ theme }) => ({
+    '&:nth-of-type(odd)': {
+        backgroundColor: theme.palette.action.hover
+    },
+    // hide last border
+    '&:last-child td, &:last-child th': {
+        border: 0,
+    },
+}));
+
+function createData(
+    name: string,
+    description: string
+) {
+    return { name, description };
+}
+
+const rows = [
+    createData('Lot Area | Coverage', 'Test'),
+    createData('Site Coverage', 'Test'),
+    createData('FAR (Floor Area Ratio)', 'Test'),
+    createData('Open Space & Landscaping', 'Test'),
+    createData('Front yard open space', 'Test'),
+    createData('Parking', 'Test'),
+    createData('Flood Zone', 'Test'),
+    createData('Noticing', 'Test'),
+
+];
+
+export default function ZoningRegulations() {
+    return (
+        <TableContainer component={Paper} style={{marginTop: '30px', width: '90%', margin: 'auto', borderStyle: "none"}}>
+      <Table sx={{ minWidth: 700 }} aria-label="customized table">
+        <TableHead>
+          <TableRow>
+            <StyledTableCell></StyledTableCell>
+            <StyledTableCell align="left"></StyledTableCell>
+            <StyledTableCell></StyledTableCell>
+            <StyledTableCell></StyledTableCell>
+            <StyledTableCell></StyledTableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <StyledTableRow key={row.name}>
+              <StyledTableCell component="th" scope="row">
+                {row.name}
+              </StyledTableCell>
+              <StyledTableCell align="left">{row.description}</StyledTableCell>
+              <StyledTableCell>{row.fat}</StyledTableCell>
+              <StyledTableCell>{row.carbs}</StyledTableCell>
+              <StyledTableCell>{row.protein}</StyledTableCell>
+            </StyledTableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+    );
+}
